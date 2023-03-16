82c5ec7e860c27056107e31be7d9d61fb9722168
AdamBenDev <adamaya9732@gmail.com>
2023-03-16 16:52:59 +0100

    while cycle

---
+++ main.js
@@ -0,0 +1,11 @@
+/*Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.*/
+
+//*Lista della spesa
+
+let listaSpesa = ["Pomodori", "Cipolle", "Insalata", "Pasta" , "Pane" ]
+
+
+//*ciclo while per stampare gli elementi della lista uno per uno 
+let i = 0;
+
+while (i < listaSpesa.lenght)
